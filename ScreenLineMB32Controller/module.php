5d2a3c0ff84aed1a2c4e51c370a942e67b002309
johannes-veit <joh.veit@googlemail.com>
2026-07-21 16:22:33 +0200

    module.php mit Steuerungsgrundlage integriert

--- ScreenLineMB32Controller/module.php
+++ ScreenLineMB32Controller/module.php
@@ -1,29 +1,128 @@
 <?php
 
+declare(strict_types=1);
+
+require_once __DIR__ . '/libs/RelayEngine.php';
+require_once __DIR__ . '/libs/SafetyEngine.php';
+require_once __DIR__ . '/libs/TrackingEngine.php';
+require_once __DIR__ . '/libs/ShakeFree.php';
+
+
 class ScreenLineMB32Controller extends IPSModule
 {
     public function Create()
     {
         parent::Create();
 
+
+        /*
+         * Hardware
+         */
+
         $this->RegisterPropertyInteger(
-            'ModuleID',
+            'RelayUp',
             0
         );
 
         $this->RegisterPropertyInteger(
-            'MotorRuntime',
-            30
+            'RelayDown',
+            0
         );
 
+
+        /*
+         * Motorlaufzeiten
+         */
+
+        $this->RegisterPropertyFloat(
+            'RuntimeUp',
+            180.0
+        );
+
+        $this->RegisterPropertyFloat(
+            'RuntimeDown',
+            180.0
+        );
+
+
+        /*
+         * Sicherheit
+         */
+
+        $this->RegisterPropertyInteger(
+            'SwitchPause',
+            400
+        );
+
+        $this->RegisterPropertyFloat(
+            'MotorTimeout',
+            190.0
+        );
+
+
+        /*
+         * Shake Free
+         */
+
         $this->RegisterPropertyBoolean(
-            'EnableSafety',
+            'ShakeFreeEnabled',
             true
         );
 
-        $this->RegisterPropertyBoolean(
-            'EnableAdvanced',
-            false
+        $this->RegisterPropertyFloat(
+            'ShakeFreeDuration',
+            1.5
+        );
+
+
+        /*
+         * interne Werte
+         */
+
+        $this->RegisterAttributeFloat(
+            'Position',
+            0.0
+        );
+
+        $this->RegisterAttributeFloat(
+            'Lamellen',
+            0.0
+        );
+
+
+        /*
+         * Visualisierung
+         */
+
+        $this->RegisterVariableInteger(
+            'BlindControl',
+            'Position',
+            '~Intensity.100',
+            10
+        );
+
+        $this->EnableAction(
+            'BlindControl'
+        );
+
+
+        $this->RegisterVariableInteger(
+            'SlatPosition',
+            'Lamellen',
+            '~Intensity.100',
+            20
+        );
+
+        $this->EnableAction(
+            'SlatPosition'
+        );
+
+
+        $this->RegisterVariableString(
+            'Status',
+            'Status',
+            '',
+            30
         );
     }
 
@@ -32,28 +131,74 @@ class ScreenLineMB32Controller extends IPSModule
     {
         parent::ApplyChanges();
 
-        $this->SetSummary(
-            'ScreenLine MB32 Controller'
+
+        $this->SetValue(
+            'Status',
+            'Bereit'
         );
     }
 
 
-    public function TestMovement()
-    {
+    public function RequestAction(
+        $Ident,
+        $Value
+    ) {
+
+        switch ($Ident) {
+
+
+            case 'BlindControl':
+
+                $this->MoveToPosition(
+                    (float)$Value
+                );
+
+                break;
+
+
+            case 'SlatPosition':
+
+                $this->MoveSlats(
+                    (float)$Value
+                );
+
+                break;
+        }
+    }
+
+
+    private function MoveToPosition(
+        float $position
+    ): void {
+
         $this->SendDebug(
             'ScreenLine',
-            'Testbewegung gestartet',
+            'Zielposition: ' . $position,
             0
+        );
+
+
+        $this->SetValue(
+            'Status',
+            'Fahrt vorbereitet'
         );
     }
 
 
-    private function SendCommand($command)
-    {
+    private function MoveSlats(
+        float $position
+    ): void {
+
         $this->SendDebug(
-            'Command',
-            $command,
+            'ScreenLine',
+            'Lamellenziel: ' . $position,
             0
+        );
+
+
+        $this->SetValue(
+            'Status',
+            'Lamellenfahrt vorbereitet'
         );
     }
 }
